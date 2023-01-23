c0350de268214061ec46fa730c7a63cf21856926
Viktoriia Nekrasova <vvinebrand@gmail.com>
2023-01-23 17:21:35 +0300

    added word output

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ print('Enter the words you want to add to the array separated by space: ', end="
 arr = input().split()
 print(f'\nYour array: {arr}\n')
 words = [i for i in arr if len(i) <= 3]
+if len(words) >= 1:
+    print('Words whose length is less than or equal to 3: ', end="")
+    print(*words, sep=', ')
+else:
+    print('There are no words in the array whose length is less than or equal to 3!')
 
