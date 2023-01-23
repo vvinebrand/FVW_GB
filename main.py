72f95c2cd6b1d5457705b550435f59400882a284
Viktoriia Nekrasova <vvinebrand@gmail.com>
2023-01-23 17:36:25 +0300

    the real final version of the code

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ print(f'\nYour array: {arr}\n')
 words = [i for i in arr if len(i) <= 3]
 if len(words) >= 1:
     print('Words whose length is less than or equal to 3: ', end="")
-    print(*words, sep=', ')
+    print(list(words))
 else:
     print('There are no words in the array whose length is less than or equal to 3!')
