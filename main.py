b102653809c5e5186c1cf738aecacdfa7092d490
Viktoriia Nekrasova <vvinebrand@gmail.com>
2023-01-23 17:13:27 +0300

    entered and split the array by lines

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Задача: Написать программу, которая из имеющегося массива формирует массив из строк, длина которых >= 3-м символам.
 
+print('Enter the words you want to add to the array separated by space: ', end="")
+arr = input().split()
+print(f'\nYour array: {arr}\n')
 
